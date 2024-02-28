991d1c2c42aa46fe90968432ab65c6b417df0e86
EsmeraldaMendo <mendozae0818@gmail.com>
2024-02-27 19:49:19 -0500

    helloWorld
    First step, How to remove line codes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,9 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Hello everyone 
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">How are you?</a> 
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Fine</a>. Thank you
     </h3>
   </div>
 </template>
